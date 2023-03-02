5df006c62eba7b407246970dc1d570b5edb1ab95
fledebaye <fledebaye@yahoo.fr>
2023-03-02 02:48:15 +0000

    premier question

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
 let nbQuestions = 3;
 console.log(nbQuestions);
+alert("Bienvenue sur ce question ! " + "\n" + nbQuestions + " Vous seront posées");
+let reponseUser = prompt(`Question 1/3 
+quel est l'age du capitaine? 
+1: 45
+2: 34
+3: 55`);
+
+console.log(reponseUser);
