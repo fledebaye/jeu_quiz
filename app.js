389d52fda07a065e04138aeea78457e851c13cb3
fledebaye <fledebaye@yahoo.fr>
2023-03-02 02:58:44 +0000

    Mes premieres conditions pour la bonne reponse

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 let nbQuestions = 3;
-console.log(nbQuestions);
+
 alert("Bienvenue sur ce question ! " + "\n" + nbQuestions + " Vous seront posées");
 let reponseUser = prompt(`Question 1/3 
 quel est l'age du capitaine? 
 1: 45
 2: 34
 3: 55`);
+let bonneReponse = 1;
 
 console.log(reponseUser);
+if(reponseUser == bonneReponse) {
+
+    console.log("Bonne reponse");
+}else{
+    console.log('Tu es nul!');
+}
